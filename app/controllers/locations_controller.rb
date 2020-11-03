3453114d126a8615ca1c3e14141e37b713b68256
Beatriz <59659925+bearescu@users.noreply.github.com>
2020-11-03 10:29:19 +0100

    Update locations_controller.rb

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,37 +1,44 @@
 class LocationsController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:index, :show]
-  # before_action :set_location, only: [:show, :edit, :update]
+  before_action :set_location, only: [:show, :edit, :update]
 
   def index
     @locations = Location.all
   end
 
-  # def show
-  # end
+  def show
+    set_location
+  end
 
-  # def edit
-  # end
+  def edit
+    set_location
+  end
 
-  # def update
-  # end
+  def update
+    set_location
+    @location.update(location_params)
+    redirect_to location_path(@location)
+  end
 
-  # def new
-  # end
+  def new
+    @location = Location.new 
+  end
 
-  # def create
-  # end
+  def create  
+    @location = Location.new(location_params)
+    @location.save
+    redirect_to location_path(@location)
+  end
 
-  # private
+  private
 
-  # def location_params
-  #   params.require(:location).permit(
-  #     :name, :price, :address, :description, :extra_id, :space, :capacity, :category_id,
-  #     available_weekdays: [], photos: []
-  #   )
-  # end
+  def location_params
+    params.require(:location).permit(:name, :price, :address, :description, :extra_id, :space, :capacity, :category_id,
+      available_weekdays: [], photos: [])
+  end
 
-  # def set_location
-  #   @location = Location.find(params[:id])
-  # end
+  def set_location
+    @location = Location.find(params[:id])
+  end
 
 end
